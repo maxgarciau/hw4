414af5d3aa4c81e898373b377bb596945980aedb
maxgarciau <maximiliano.garcia@kellogg.northwestern.edu>
2022-06-01 02:32:49 +0000

    auth1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,25 @@
 class SessionsController < ApplicationController
-  def new
-  end
-
   def create
+    @user = User.find_by({ "email" => params["email"] })
+    if @user
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "Welcome"
+        redirect_to "/posts"
+      else
+        flash["notice"] = "Wrong password"
+        redirect_to "/login"
+      end
+    else
+      flash["notice"] = "User doesn't exists"
+      redirect_to "/login"
+    end
   end
 
   def destroy
+    session["user_id"] = nil
+    flash["notice"] = "Logged off"
+    redirect_to "/login"
   end
 end
   
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,10 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new
-    @user["first_name"] = params["user"]["first_name"]
-    @user["last_name"] = params["user"]["last_name"]
+    @user = User.create
+    @user["username"] = params["user"]["username"]
     @user["email"] = params["user"]["email"]
-    @user["password"] = params["user"]["password"]
+    @user["password"] = BCrypt::Password.create(params["user"]["password"])
     @user.save
     redirect_to "/"
   end
